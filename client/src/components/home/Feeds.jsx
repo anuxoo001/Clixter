3bb4f53c6e475db6119a97b55b7b1d92f0d0320b
Anu <anuxoo001@gmail.com>
2026-05-28 19:29:42 +0530

    Connect frontend to Render backend and update deployment config

--- client/src/components/home/Feeds.jsx
+++ client/src/components/home/Feeds.jsx
@@ -4,29 +4,8 @@ import Posts from './Posts';
 import { useSelector } from 'react-redux';
 
 const Feeds = () => {
-  // const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const {posts} = useSelector(store => store.post)
-
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     try {
-  //       const res = await axios.get('http://localhost:5000/api/post/getallposts', {
-  //         withCredentials: true,
-  //       });
-
-  //       if (res.data.success) {
-  //         setPosts(res.data.posts);
-  //       } 
-  //     } catch (err) {
-  //       console.error(err);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchPosts();
-  // }, []);
 
   return (
     <div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3004',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/socket.io': {
-        target: 'http://localhost:3004',
-        ws: true,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const target = env.VITE_API
+
+  return defineConfig({
+    plugins: [react()],
+    server: target
+      ? {
+          proxy: {
+            '/api': {
+              target,
+              changeOrigin: true,
+              secure: false,
+            },
+            '/socket.io': {
+              target,
+              ws: true,
+              changeOrigin: true,
+              secure: false,
+            },
+          },
+        }
+      : undefined,
+  })
+}
