2424c95c053af3e064c2e1b9a5450772d60103e3
Anu <anuxoo001@gmail.com>
2026-06-06 20:22:44 +0530

    feat(post-scheduling): add scheduling fields, API, worker, and UI

--- server/src/routes/post.route.js
+++ server/src/routes/post.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addCommentOnPost, addPost, bookmarkToPost, deletePost, editCommentOnPost, getAllPost, getCommentOfPost, getUserPost, likeDislikeToPost, deleteCommentOnPost } from "../controllers/post.controller.js";
+import { addCommentOnPost, addPost, bookmarkToPost, deletePost, editCommentOnPost, getAllPost, getCommentOfPost, getUserPost, likeDislikeToPost, deleteCommentOnPost, schedulePost } from "../controllers/post.controller.js";
 import { reactToComment, reactToPost } from "../controllers/reaction.controller.js";
 import verifyToken from "../middlewares/verifyToken.js"
 import upload from "../middlewares/multer.js";
@@ -7,6 +7,7 @@ import upload from "../middlewares/multer.js";
 const router = express.Router();
 
 router.post('/addpost' , verifyToken, upload.single('media') , addPost);
+router.post('/schedule' , verifyToken, upload.single('media') , schedulePost);
 router.delete('/delete/:id' , verifyToken, deletePost);
 router.get('/getallposts' , verifyToken, getAllPost);
 router.get('/getuserspost' , verifyToken, getUserPost);
